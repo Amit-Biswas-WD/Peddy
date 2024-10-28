7c3f912cfd0c587174ff1ee92b4739a5eaaeb116
Amit Biswas <amitbiswas9282@gmail.com>
2024-10-28 21:02:39 +0600

    Category Button

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("I Love You Anu...");
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategory(data.categories))
+    .catch((error) => console.log(error));
+};
+
+const displayCategory = (category) => {
+  const categoryContainer = document.getElementById("display-categories");
+  category.forEach((item) => {
+    console.log(item);
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `<div>
+    
+    <button id="${item.id}" class="btn btn-outline btn-accent w-60 h-16 gap-8"><img id="${item.id}"
+      src=${item.category_icon}
+      class="w-10"
+      alt="Shoes" />${item.category}</button>
+    </div>`;
+    categoryContainer.append(buttonContainer);
+  });
+};
+
+loadCategory();
